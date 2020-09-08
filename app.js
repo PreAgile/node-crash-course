6346197a134dc4f10a6d67ca65f5afceef82a567
Myun <digle117@gmail.com>
2020-09-08 10:24:23 +0900

    Middleware

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const morgan = require('morgan');
 // express app
 const app = express();
 
@@ -9,25 +9,45 @@ app.set('view engine', 'ejs');
 // listen for requests
 app.listen(3000);
 
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  console.log('new request mdae:');
+  console.log('host: ', req.hostname);
+  console.log('path: ', req.path);
+  console.log('method: ', req.method);
+  next();
+});
+
 app.get('/', (req, res) => {
   const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {
+      title: 'Yoshi finds eggs',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'Mario finds stars',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'How to defeat bowser',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
   ];
-  res.render('index', { title : 'Home', blogs});
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { title : 'About'});
+  res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create', { title : 'Create a new Blog'});
-})
-
+  res.render('create', { title: 'Create a new Blog' });
+});
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title : '404'});
+  res.status(404).render('404', { title: '404' });
 });
